e7a0ffc96d3994b6f409b2802317d639031089b8
George Priniski <priniskigeorge@gmail.com>
2016-05-10 20:09:40 +0000

    Add validations for address and description, modify validation for plac

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,11 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true, length: { :within => 3..30 }
+  validates :address, presence: true, length: { :within => 3..100 }
+  validates :description, presence: true, length: { :within => 3..500 }
 end
+
+ #class Film <; ActiveRecord::Base
+  #validates :title, :presence => true, :uniqueness => true, :length => { :maximum => 100 }
+  #validates :budget, :presence => true, :length => { :within => 1..10000000 }
+#end
