eed9f9c6dcee0a48256a3f9558fcb515f458c02e
George Priniski <priniskigeorge@gmail.com>
2016-05-18 05:46:36 +0000

    Add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Nomster::Application.routes.draw do
-
+  
   devise_for :users
+  
   root 'places#index'
 
   # Part of project. Add standard resources for places.
@@ -8,7 +9,9 @@ Nomster::Application.routes.draw do
     resources :comments, only: :create
     resources :photos, only: :create
   end
+  
 
+  resources :users, only: :show
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
